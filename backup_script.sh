ee5ff776db745c4e4918b24d1eb760747524da27
osmanjaffery <masterpozz1388@gmail.com>
2015-12-13 20:13:42 -0500

    removed bash script

--- backup_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-THEDATE=`date +%d%m%y%H%M`
-
-tar -czf /home/ozzy/backups/frontend_${THEDATE}.tar.gz /var/www/html/frontend
-
-find /home/ozzy/backups/frontend* -mtime +1 -exec rm {} \;
